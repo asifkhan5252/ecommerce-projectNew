2e3906685e8af1ab04d19e9ffeb08fbe68319b38
asifkhan5252 <akhan87372@gmail.com>
2025-10-24 20:52:48 +0530

    footer some change

--- client/src/components/Layout/Footer.jsx
+++ client/src/components/Layout/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="footer">
-      <h1 className="text-center text-xs">All Right Reserved &copy; Techinfoyt</h1>
+      <h1 className="text-center text-xs">All Right Reserved &copy; Asif Khan</h1>
       <p className="text-center mt-3">
        <Link className="footer-link" to="/about">About</Link>|
     <Link className="footer-link" to="/contact">Contact</Link>|
